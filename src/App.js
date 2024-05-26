fcac58ce7022d1bbbe52ce020a1b41135c52bfb1
Olga <minaeva9@gmail.com>
2024-05-26 18:47:33 -0400

    create server for data and useEffect load data on mount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
